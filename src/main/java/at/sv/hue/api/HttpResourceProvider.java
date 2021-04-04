74a4d160604bb1da8c322ef20f100dff6b999a16
Stefan Victora <stefan.victora@gmail.com>
2021-04-04 11:41:45 +0200

    Adds method contract to HttpResourceProvider interface

--- src/main/java/at/sv/hue/api/HttpResourceProvider.java
+++ src/main/java/at/sv/hue/api/HttpResourceProvider.java
@@ -3,7 +3,14 @@ package at.sv.hue.api;
 import java.net.URL;
 
 public interface HttpResourceProvider {
+    /**
+     * @return the requested resource as string, or null if an error occurred
+     */
     String getResource(URL url);
 
+    /**
+     * @param body the json payload of the put request
+     * @return the response of the server, or null if an error occurred
+     */
     String putResource(URL url, String body);
 }
