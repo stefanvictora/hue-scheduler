44465bc788f2044f946a2d13700250cae8045a04
Stefan Victora <stefan.victora@gmail.com>
2025-04-06 12:25:51 +0200

    Revert "Fix timing issue with wasJustTurned"
    This reverts commit adf3478a24899147f4f9c771942d0db1bdcd2b08.

--- src/main/java/at/sv/hue/api/ManualOverrideTrackerImpl.java
+++ src/main/java/at/sv/hue/api/ManualOverrideTrackerImpl.java
@@ -1,10 +1,12 @@
 package at.sv.hue.api;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.Ticker;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Map;
+import java.time.Duration;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ManualOverrideTrackerImpl implements ManualOverrideTracker {
@@ -12,22 +14,21 @@ public class ManualOverrideTrackerImpl implements ManualOverrideTracker {
     private static final TrackedState DEFAULT_STATE = new TrackedState();
 
     private final ConcurrentHashMap<String, TrackedState> trackedStatesPerId;
-    private final Map<String, Long> lastTurnedOnTimePerId;
-    private final long justTurnedOnWindowDuration;
-    private final Ticker ticker;
+    private final Cache<String, String> wasJustTurnedOnIds;
 
     public ManualOverrideTrackerImpl(Ticker ticker, int justTurnedOnWindowInSeconds) {
-        this.ticker = ticker;
-        justTurnedOnWindowDuration = justTurnedOnWindowInSeconds * 1_000_000_000L;
         trackedStatesPerId = new ConcurrentHashMap<>();
-        lastTurnedOnTimePerId = new ConcurrentHashMap<>();
+        wasJustTurnedOnIds = Caffeine.newBuilder()
+                                     .ticker(ticker)
+                                     .expireAfterWrite(Duration.ofSeconds(justTurnedOnWindowInSeconds))
+                                     .build();
     }
 
     @Override
     public void onManuallyOverridden(String id) {
         TrackedState trackedState = getOrCreateTrackedState(id);
         trackedState.setManuallyOverridden(true);
-        resetJustTurnedOn(id);
+        wasJustTurnedOnIds.invalidate(id);
         trackedState.setTurnedOnBySyncedScene(false);
     }
 
@@ -41,7 +42,7 @@ public class ManualOverrideTrackerImpl implements ManualOverrideTracker {
         TrackedState trackedState = getOrCreateTrackedState(id);
         trackedState.setManuallyOverridden(false);
         trackedState.setLightIsOff(false);
-        lastTurnedOnTimePerId.put(id, ticker.read());
+        wasJustTurnedOnIds.put(id, id);
     }
 
     @Override
@@ -55,7 +56,7 @@ public class ManualOverrideTrackerImpl implements ManualOverrideTracker {
         TrackedState trackedState = getOrCreateTrackedState(id);
         trackedState.setLightIsOff(true);
         trackedState.setTurnedOnBySyncedScene(false);
-        resetJustTurnedOn(id);
+        wasJustTurnedOnIds.invalidate(id);
     }
 
     @Override
@@ -65,8 +66,7 @@ public class ManualOverrideTrackerImpl implements ManualOverrideTracker {
 
     @Override
     public boolean wasJustTurnedOn(String id) {
-        Long last = lastTurnedOnTimePerId.get(id);
-        return last != null && ticker.read() - last < justTurnedOnWindowDuration;
+        return wasJustTurnedOnIds.getIfPresent(id) != null;
     }
 
     @Override
@@ -78,7 +78,7 @@ public class ManualOverrideTrackerImpl implements ManualOverrideTracker {
     public void onAutomaticallyAssigned(String id) {
         TrackedState trackedState = getOrCreateTrackedState(id);
         trackedState.setManuallyOverridden(false);  // maybe not needed, as this flag is overridden also on light-on events
-        resetJustTurnedOn(id);
+        wasJustTurnedOnIds.invalidate(id);
     }
 
     private TrackedState getOrCreateTrackedState(String id) {
@@ -87,10 +87,6 @@ public class ManualOverrideTrackerImpl implements ManualOverrideTracker {
 
     private TrackedState getOrDefaultState(String id) {
         return trackedStatesPerId.getOrDefault(id, DEFAULT_STATE);
-    }
-
-    private void resetJustTurnedOn(String id) {
-        lastTurnedOnTimePerId.remove(id);
     }
 
     @Getter
