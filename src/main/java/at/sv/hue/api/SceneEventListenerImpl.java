81b73721941488ffc3d1630a1eb47b1c10e7208d
Stefan Victora <stefan.victora@gmail.com>
2025-04-06 11:41:00 +0200

    Use Set for affectedIdsForScene to avoid repeated turnOns

--- src/main/java/at/sv/hue/api/SceneEventListenerImpl.java
+++ src/main/java/at/sv/hue/api/SceneEventListenerImpl.java
@@ -7,8 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -42,7 +42,7 @@ public final class SceneEventListenerImpl implements SceneEventListener {
         MDC.put("context", "scene-on-event " + id);
         String sceneName = hueApi.getSceneName(id);
         MDC.put("context", "scene-on-event " + sceneName);
-        List<String> affectedIdsByScene = getAffectedIdsByScene(id);
+        Set<String> affectedIdsByScene = getAffectedIdsForScene(id);
         if (isSyncedScene(sceneName)) {
             log.info("Synced scene activated. Re-engage scheduler.");
             affectedIdsByScene.forEach(lightOrGroupId -> recentlyAffectedSyncedIds.put(lightOrGroupId, lightOrGroupId));
@@ -52,17 +52,16 @@ public final class SceneEventListenerImpl implements SceneEventListener {
         affectedIdsByScene.forEach(lightOrGroupId -> recentlyAffectedIds.put(lightOrGroupId, lightOrGroupId));
     }
 
-    private List<String> getAffectedIdsByScene(String sceneId) {
-        List<String> affectedIdsByScene = new ArrayList<>(hueApi.getAffectedIdsByScene(sceneId));
-        affectedIdsByScene.addAll(getAdditionalAffectedIds(affectedIdsByScene));
-        return affectedIdsByScene;
+    private Set<String> getAffectedIdsForScene(String sceneId) {
+        Set<String> affectedIds = new HashSet<>(hueApi.getAffectedIdsByScene(sceneId));
+        affectedIds.addAll(fetchAdditionalGroupsForLights(affectedIds));
+        return affectedIds;
     }
 
-    private List<String> getAdditionalAffectedIds(List<String> ids) {
-        return ids.stream()
-                  .flatMap(light -> hueApi.getAssignedGroups(light).stream())
-                  .distinct()
-                  .collect(Collectors.toList());
+    private Set<String> fetchAdditionalGroupsForLights(Set<String> lightIds) {
+        return lightIds.stream()
+                       .flatMap(lightId -> hueApi.getAssignedGroups(lightId).stream())
+                       .collect(Collectors.toSet());
     }
 
     private boolean isSyncedScene(String sceneName) {
