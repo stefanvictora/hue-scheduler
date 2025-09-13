6c0b8269d602f720da78bfffb1da65f183b2f5a2
Stefan Victora <stefan.victora@gmail.com>
2025-09-13 17:18:34 +0200

    Don't interpolate between equal gradients.
    Don't allow gradient properties for scenes.

--- src/main/java/at/sv/hue/Gradient.java
+++ src/main/java/at/sv/hue/Gradient.java
@@ -3,9 +3,22 @@ package at.sv.hue;
 import lombok.Builder;
 
 import java.util.List;
+import java.util.Objects;
 
 @Builder
 public record Gradient(List<Pair<Double, Double>> points, String mode) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Gradient gradient = (Gradient) o;
+        return Objects.equals(mode, gradient.mode) && Objects.equals(points, gradient.points);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(points, mode);
+    }
 
     @Override
     public String toString() {
