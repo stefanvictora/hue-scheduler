31ddec1900fe32cdc0ecbb46ec500686b82ed3fe
Stefan Victora <stefan.victora@gmail.com>
2026-04-09 21:05:00 +0200

    Bump version

--- src/main/java/at/sv/hue/HueScheduler.java
+++ src/main/java/at/sv/hue/HueScheduler.java
@@ -62,7 +62,7 @@ import java.util.stream.Stream;
 
 import static at.sv.hue.InputConfigurationParser.parseBrightnessPercentValue;
 
-@Command(name = "HueScheduler", version = "0.16.1", mixinStandardHelpOptions = true, sortOptions = false)
+@Command(name = "HueScheduler", version = "0.17.0-SNAPSHOT", mixinStandardHelpOptions = true, sortOptions = false)
 public final class HueScheduler implements Runnable {
 
     private static final Logger LOG = LoggerFactory.getLogger(HueScheduler.class);
